1f0c753553a5712cbd33fe77d2c2b1370de36913
Beytullah Arslan <beytullah.ars@hotmail.com>
2022-08-28 22:36:19 +0300

    versiyon2

--- Program.cs
+++ Program.cs
@@ -1,85 +1,168 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-class Program{
+//  class Program{
 
+//  }
+// class Person
+// {
+// // public string isim;
 
-static void Main(string[] args){
-        string kullanıcı_adı ="admin";
-        string kullanıcı_soyad="admin";
-        int parola=2525;
-        int bakiye=25000;
+//     // public int yas;
 
-        Console.WriteLine("ATM");
-        Console.WriteLine("*********************");
-        Console.Write("\t\t\t\tUsername : ");
-        string ad=Console.ReadLine();
-        Console.Write("\t\t\t\tUser Last Name : ");
-        string soyad=Console.ReadLine();
-        Console.Write("\t\t\t\tPassword : ");
+//     // public void bilgiAl(){
 
-        int sifre =int.Parse(Console.ReadLine());
+//     //     Console.WriteLine("isim : " + this.isim);
+//     //     Console.WriteLine("Yaş : " + this.yas);
 
-        if (kullanıcı_adı.Equals(ad) && kullanıcı_soyad.Equals(soyad) && (parola.Equals(sifre)))
+//     // }
+// }
+// class Araba{
+// public string Marka { get; set; }
+// public string Model { get; set; }
+// public string Renk { get; set; }
+// public bool Otomatik { get; set; }
+
+// public void Start(){
+//     Console.WriteLine(this.Marka + " " + this.Model + " çalıştırıldı.");
+// }
+
+// public void Stop(){
+//     Console.WriteLine(this.Marka + " " + this.Model + " stop edildi");
+// }
+// public void Yavasla(){
+//     Console.WriteLine(this.Marka + " " + this.Model + " yavaşlıyor");
+// }
+// public void Hızlan(){
+//     Console.WriteLine(this.Marka + " " + this.Model + " hızlanıyor");
+// }
+
+// public void Menu(){
+//     string komut="";
+//     do
+//     {
+//             Console.WriteLine("1:Start\t 2:Stop\t 3:Yavaşla\t 4:Hızlan\t Çıkış:Ç ");
+//             Console.Write("Seçiminiz : ");
+//              komut=Console.ReadLine();
+
+//              switch (komut)
+//              {
+//                 case "1" :
+//                 this.Start();
+//                 break;
+//                 case "2" :
+//                 this.Stop();
+//                 break;
+//                 case "3" :
+//                 this.Yavasla();
+//                 break;
+//                 case "4" :
+//                 this.Hızlan();
+//                 break;
+//                 default:
+//                 Console.WriteLine("Uygulamadan çıkıldı. ");
+//                 break;
+//              }
+//     } while (komut!="Ç");
+
+// }
+
+class Comment
+    {
+        public int CommentId { get; set; }
+        public string Text { get; set; }
+    }
+
+    class Product
+    {
+        public Product()
         {
-            
-                Console.WriteLine("Bankamıza Hoşgeldiniz");
-                Console.WriteLine("************************");
-                Console.WriteLine("1.Bakiye Öğrenme");
-                Console.WriteLine("2.Para Yatırma");
-                Console.WriteLine("3.Para Çekme");
-                Console.WriteLine("Çıkış için q'a basınız");
+            this.ProductId = (new Random()).Next(11111,99999);
+            this.Comments = new Comment[3];
+        }
 
-        }
-        else
+        public Product(int productId):this()
         {
-            Console.WriteLine("Yanlış bilgi girdiniz.");
-            
+            this.ProductId = productId;
         }
+
+        public Product(int productId,string name,double price,bool isApproved):this(productId)
+        {
+            this.Name = name;
+            this.Price=price;
+            this.IsApproved=isApproved;
+        }
+
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool IsApproved { get; set; }
+        public Comment[] Comments { get; set; }
     
-        while (true)
-        {
-            Console.WriteLine("İşlem seçiniz : ");
-            string secim=Console.ReadLine();
-            if (secim=="1")
-            {
-                Console.WriteLine("Bakiyeniz : " + bakiye + "TL");
-            }
-            if (secim=="2")
-            {
-                Console.WriteLine("Yatırmak istediğiniz tutarı giriniz : ");
-                int tutar=int.Parse(Console.ReadLine());
-                bakiye +=tutar;
-                Console.WriteLine("Yeni Bakiyeniz : " + bakiye + " TL");
 
-            }
-            if (secim=="3")
-            {
-                Console.WriteLine("Çekmek istediğiniz tutarı giriniz : ");
-                int tutar=int.Parse(Console.ReadLine());
-                
-                if (bakiye-tutar<0)
-                {
-                    Console.WriteLine("Yetersiz bakiye");
-                }
-                else
-                {
-                    bakiye -=tutar;
-                    Console.WriteLine("Yeni bakiyeniz : " + bakiye + " TL ");
-                }
-            }
-            if (secim=="q")
-            {
-                Console.WriteLine("Çıkış yapılıyor...");
-                break;
-            }
-            else
-            {
-                Console.WriteLine("Geçersiz bilgi lütfen tekrar deneyiniz ");
-            } 
+
+
+    static void Main(string[] args)
+    {
+        var c1 = new Comment { CommentId=1,Text="Güzel telefon"};
+
+            var p1 = new Product();
+
+            p1.Comments[0] = c1;
+
+            Console.WriteLine(p1.ProductId);            
+            Console.WriteLine(p1.Name);            
+            Console.WriteLine(p1.Price);            
+            Console.WriteLine(p1.IsApproved);         
+            Console.WriteLine(p1.Comments[0].Text);         
+
+            Console.WriteLine("*********************");
+
+            var p2 = new Product(1213);
+
+            p2.Comments[0] = c1;
+
+            Console.WriteLine(p2.ProductId);            
+            Console.WriteLine(p2.Name);            
+            Console.WriteLine(p2.Price);            
+            Console.WriteLine(p2.IsApproved); 
+            Console.WriteLine(p2.Comments[0].Text);       
+
+            Console.WriteLine("*********************");
             
+            var p3 = new Product(1231,"samsung s7",3000,true);
+
+            Console.WriteLine(p3.ProductId);            
+            Console.WriteLine(p3.Name);            
+            Console.WriteLine(p3.Price);            
+            Console.WriteLine(p3.IsApproved);   
+
+            p3.Comments[0] = c1;
+            Console.WriteLine(p3.Comments[0].Text);  
+
         }
+    }
+
+
+        // var araba=new Araba();
+        // araba.Marka="Mercedes";
+        // araba.Model="ML-350";
+        // araba.Renk="Siyah";
+        // araba.Otomatik=true;
+
+        // araba.Menu();
 
 
 
-}
-}
+        //  Person p1=new Person();
+        // p1.isim="hümeyra";
+        // p1.yas=14;
+
+        //  Person p2=new Person();
+        //   p2.isim="belinay";
+        // p2.yas=13;
+        // p1.bilgiAl();
+        // p2.bilgiAl();
+
+    
+
+    
